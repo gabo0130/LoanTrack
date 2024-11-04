d16065c3c330190139d4f58e2bf4f757fc0e8df5
Jhony Quintero <54184615+gabo0130@users.noreply.github.com>
2024-11-04 14:38:09 -0500

    Base Project

---
+++ backend/LoanTrack-Backend/src/main/java/com/LoanTrack/Backend/LoanTrackBackendApplication.java
@@ -0,0 +1,12 @@
+package com.LoanTrack.Backend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LoanTrackBackendApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(LoanTrackBackendApplication.class, args);
+    }
+}
